9aa8c2f732d86aabebd33f0c87898f3cfde116e3
andev <andh1006@naver.com>
2025-11-06 10:18:43 +0900

    Init project

---
+++ sesac-gittest/sesac-gittest/sesac_gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  sesac_gittestApp.swift
+//  sesac-gittest
+//
+//  Created by andev on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct sesac_gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
